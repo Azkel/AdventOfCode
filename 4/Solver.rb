1a04cb077f64be27656ee1a6e4ed08cbf86e8556
Michał Smyk <michal@smyk.it>
2015-12-04 08:30:41 +0100

    Day 4

---
+++ 4/Solver.rb
@@ -0,0 +1,17 @@
+input = ARGV[0]
+require 'digest'
+currentValue = 1
+found = false
+md5 = Digest::MD5.new
+while not found
+    currentValue += 1
+    input+currentValue.to_s
+    result = md5.hexdigest(input+currentValue.to_s)
+    if result[0,6] == '000000' 
+        puts result
+        found = true
+    end
+   
+end
+puts currentValue
+
