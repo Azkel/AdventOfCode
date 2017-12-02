86e6ef6c4f897f8abe3a1c04cdd136e459704680
Michał Smyk <s.michalsmyk@gmail.com>
2017-12-02 01:54:02 +0100

    Day 1 completed

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode.Days;
+using System;
+using AdventOfCode.Enums;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Day1.GetResult(TaskNumber.Second));
+            Console.ReadLine();
+        }
+    }
+}
