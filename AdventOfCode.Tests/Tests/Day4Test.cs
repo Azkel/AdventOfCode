98633db9143a5086f8f4da557e7e1ee1fd541570
Michał Smyk <s.michalsmyk@gmail.com>
2017-12-04 17:58:21 +0100

    Day 4 commited

---
+++ AdventOfCode.Tests/Tests/Day4Test.cs
@@ -0,0 +1,40 @@
+﻿using AdventOfCode.Days;
+using NUnit.Framework;
+using System;
+
+namespace AdventOfCode.Tests.Tests
+{
+    [TestFixture]
+    public class Day4Test
+    {
+        [Test]
+        public void Day4_FirstTask()
+        {
+            // Arrange
+            Day4 day = new Day4();
+            day.FilePath = AppDomain.CurrentDomain.BaseDirectory + @"..\..\TestInputs\Day4FirstExample.txt";
+            int expectedResult = 2;
+
+            // Act
+            var result = day.GetResult(Enums.TaskNumber.First);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void Day4_SecondTask()
+        {
+            // Arrange
+            Day4 day = new Day4();
+            day.FilePath = AppDomain.CurrentDomain.BaseDirectory + @"../../TestInputs/Day4SecondExample.txt";
+            int expectedResult = 3;
+
+            // Act
+            var result = day.GetResult(Enums.TaskNumber.Second);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ AdventOfCode/Days/Day4.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Days
+{
+    public class Day4 : AbstractDay
+    {
+        public string FilePath = "../../Inputs/Day4.txt";
+
+        protected override int GetResultFirst()
+        {
+            int sum = 0;
+            try
+            {
+                using (StreamReader sr = new StreamReader(FilePath))
+                {
+                    string currentLine = "";
+                    while ((currentLine = sr.ReadLine()) != null)
+                    {
+                        var array = currentLine.Split(' ');
+
+                        sum += array.Distinct().Count() == array.Count() ? 1 : 0;
+                    }
+                }
+                return sum;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error occured during processing! Details:" + e.Message);
+                throw;
+            }
+        }
+
+        protected override int GetResultSecond()
+        {
+            int sum = 0;
+            try
+            {
+                using (StreamReader sr = new StreamReader(FilePath))
+                {
+                    string currentLine = "";
+                    while ((currentLine = sr.ReadLine()) != null)
+                    {
+                        var array = currentLine.Split(' ').Select(c => String.Concat(c.OrderBy(x => x)));
+                        sum += array.Distinct().Count() == array.Count() ? 1 : 0;
+                    }
+                }
+                return sum;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error occured during processing! Details:" + e.Message);
+                throw;
+            }
+        }
+    }
+}
