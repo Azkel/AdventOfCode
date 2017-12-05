a668ef28f0e9cd088db795ba4172d66f6bcc8554
Michał Smyk <s.michalsmyk@gmail.com>
2017-12-06 00:43:04 +0100

    Day 5 added. This time without any tests, as it is late night, I am sick and want to finally go to sleep.

---
+++ AdventOfCode/Days/Day5.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Days
+{
+    public class Day5 : AbstractDay
+    {
+        public string FilePath = "../../Inputs/Day5.txt";
+
+
+        protected override int GetResultFirst()
+        {
+            int sum = 0;
+            List<int> listOfValues = new List<int>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(FilePath))
+                {
+                    string currentLine = "";
+                    while((currentLine = sr.ReadLine()) != null)
+                    {
+                        listOfValues.Add(Int32.Parse(currentLine));
+                    }
+                }
+                int currentIndex = 0, jumpCount = 0;
+                var values = listOfValues.ToArray();
+                while (currentIndex >= 0 && currentIndex < values.Count())
+                {
+                    jumpCount++;
+                    values[currentIndex]++;
+                    currentIndex += (values[currentIndex] - 1);
+                }
+                return jumpCount;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error occured during processing! Details:" + e.Message);
+                throw;
+            }
+        }
+
+        protected override int GetResultSecond()
+        {
+                      int sum = 0;
+            List<int> listOfValues = new List<int>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(FilePath))
+                {
+                    string currentLine = "";
+                    while((currentLine = sr.ReadLine()) != null)
+                    {
+                        listOfValues.Add(Int32.Parse(currentLine));
+                    }
+                }
+                int currentIndex = 0, jumpCount = 0;
+                int offset = 0;
+                var values = listOfValues.ToArray();
+                while (currentIndex >= 0 && currentIndex < values.Count())
+                {
+                    jumpCount++;
+                    if(values[currentIndex] >= 3)
+                    {
+                        offset = 1;
+                        values[currentIndex]--;
+                    }
+                    else
+                    {
+                        offset = -1;
+                        values[currentIndex]++;
+                    }
+                    currentIndex += (values[currentIndex] + offset);
+                }
+                return jumpCount;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error occured during processing! Details:" + e.Message);
+                throw;
+            }
+        }
+    }
+}
