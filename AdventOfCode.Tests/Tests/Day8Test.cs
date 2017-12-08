a7c28d6ec2df60947454dcb757c94bde2ba31097
Michał Smyk <s.michalsmyk@gmail.com>
2017-12-08 09:54:16 +0100

    Day 8

---
+++ AdventOfCode.Tests/Tests/Day8Test.cs
@@ -0,0 +1,40 @@
+﻿using AdventOfCode.Days;
+using NUnit.Framework;
+using System;
+
+namespace AdventOfCode.Tests.Tests
+{
+    [TestFixture]
+    public class Day8Test
+    {
+        [Test]
+        public void Day8_FirstTask()
+        {
+            // Arrange
+            Day8 day = new Day8();
+            day.FilePath = AppDomain.CurrentDomain.BaseDirectory + @"..\..\TestInputs\Day8FirstExample.txt";
+            string expectedResult = "1";
+
+            // Act
+            var result = day.GetResult(Enums.TaskNumber.First);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void Day8_SecondTask()
+        {
+            // Arrange
+            Day8 day = new Day8();
+            day.FilePath = AppDomain.CurrentDomain.BaseDirectory + @"../../TestInputs/Day8FirstExample.txt";
+            string expectedResult = "10";
+
+            // Act
+            var result = day.GetResult(Enums.TaskNumber.Second);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
