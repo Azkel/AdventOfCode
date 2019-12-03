dac511f3998c6ddf85b7a070fb8029cfcb68c263
Michał Smyk <s.michalsmyk@gmail.com>
2019-12-03 22:26:17 +0100

    Second part of day 3 added

---
+++ 2019/BlazorAdvent/Shared/ThirdDayClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorAdvent.Shared
+{
+    public class ThirdDayClass
+    {
+        public int AmountOfCrossings = 1;
+        public int FirstInputStep = -1;
+        public int SecondInputStep = -1;
+    }
+}
